01f309eb68f3eb9768520994d8a5589d6ba9e284
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-11 12:19:02 +0300

    replace puppeteer with tor-request

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,32 +1,31 @@
 const puppeteer  = require('puppeteer');
 const cheerio = require('cheerio');
+const tr = require('tor-request');
 
-const getPaste = async (req, res) => {
+const getPaste = async () => {
     try{
-        const args = ['--proxy-server=socks5://127.0.0.1:9050']; // creates the proxy definition for puppeteer
-        const browser = await puppeteer.launch({args}); // start the browser
-        const page = await browser.newPage();
-        await page.goto('http://nzxj65x32vh2fkhk.onion/all'); // Stronghold website
-        const content = await page.content();
-        const $ = cheerio.load(content, { 
-            xml: {
-                normalizeWhitespace: true,
+        tr.request('http://nzxj65x32vh2fkhk.onion/all', function (err, res, body) {
+            if (!err && res.statusCode == 200) {
+                const $ = cheerio.load(body, { 
+                    xml: {
+                        normalizeWhitespace: true,
+                    }
+                });
+                const postList = [];
+                $('div[class = col-sm-12]').each((index, elem) => {
+                    const pasteContentNPSB = $(elem).find('.text').text();
+                    const pasteContent = pasteContentNPSB.split(/&nbsp;/);
+                    const post = {
+                        title: $(elem).find('h4').text(),
+                        author: $(elem).find('.col-sm-6').text(),
+                        content: pasteContent,
+                    }
+                    post.content[0] === '' ? null : postList.push(post); // add only pastes with content
+                });
+                console.log(postList);
             }
         });
-        const postList = [];
-        $('div[class = col-sm-12]').each((index, elem) => {
-            const pasteContentNPSB = $(elem).find('.text').text();
-            const pasteContent = pasteContentNPSB.split(/&nbsp;/);
-            const post = {
-                title: $(elem).find('h4').text(),
-                author: $(elem).find('.col-sm-6').text(),
-                content: pasteContent,
-            }
-            post.content[0] === '' ? null : postList.push(post); // add only pastes with content
-        });
-        await browser.close();
-        console.log(postList);
-    } catch(err){
+    } catch (err){
         console.log(err.message);
     }
 };
