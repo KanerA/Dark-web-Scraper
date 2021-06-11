ed74c40b0857315984d20e3667df6edecb27ef34
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-11 15:18:28 +0300

    connect to mongo on server start

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
-const app = require('./app');
+require("dotenv").config();
+const app = require("./app");
+const mongoose = require("mongoose");
+const env = process.env.NODE_ENV || "development";
+const MONGO_URI =
+  env === "test" ? process.env.TEST_MONGO_URI : process.env.MONGO_URI;
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`App is running on port ${PORT}`);
-});
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log(`connected to MongoDB - ${env}`);
+    app.listen(PORT, () =>
+      console.log(`app listening at http://localhost:${PORT}`)
+    );
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
