23656dc8f08ab285f4eb353586501ae7a8ed451b
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-11 22:25:43 +0300

    add trailing spaces function
    this function trims the contents array's trailing spaces.
    needed modification to trim author/title

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -15,11 +15,13 @@ const getPaste = async () => {
                 const postList = [];
                 $('div[class = col-sm-12]').each((index, elem) => {
                     const pasteContentNPSB = $(elem).find('.text').text();
-                    const pasteContent = pasteContentNPSB.split(/&nbsp;/);
+                    const pasteContent = pasteContentNPSB.split(/&nbsp;/); // split the content by non breaking spaces that exist.
+                    const trimmedContent = removeTrailingSpaces(pasteContent);
+                    if(!trimmedContent) return ;
                     const post = {
                         title: $(elem).find('h4').text(),
                         author: $(elem).find('.col-sm-6').text(),
-                        content: pasteContent,
+                        content: trimmedContent,
                     }
                     post.content[0] === '' ? null : postList.push(post); // add only pastes with content
                 });
@@ -31,5 +33,25 @@ const getPaste = async () => {
         console.log(err.message);
     }
 };
+    
+const removeTrailingSpaces = (strToTrim) => {
+    // --------- for title, author, date etc. ----------------- //
+    // if(typeof strToTrim === 'string'){
+    //     const trimmedStr = strToTrim.trim();
+    //     return trimmedStr;
+    // }
+    const filteredStrArr = strToTrim.filter(str => {
+        const trimmedStr = str.trim();
+        if(trimmedStr.length === 0) return ;
+        return trimmedStr;
+    });
+    if(filteredStrArr.length === 0) return null;
+    const trimmedArr = filteredStrArr.map(str => {
+        const trimmedStr = str.trim();
+        return trimmedStr;
+    })
+    return trimmedArr;
+};
 
-setInterval(getPaste, 120000);
+// setInterval(getPaste, 120000);
+setInterval(getPaste, 5000);
