88851e87936ebf7cb3a810550dbb54b953ebfed7
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-12 23:24:27 +0300

    get pastes from db

--- server/utils.js
+++ server/utils.js
@@ -1,18 +1,14 @@
 const Paste = require('./models/Paste');
 
-const newPaste = (req, res) => {
-    const pastesArr = req.body;
-    pastesArr && pastesArr.map(paste => {
-        const mongoPaste = new Paste(paste);
-        mongoPaste.save()
-        .then( _ => {
-            console.log('Success!');
-        })
-        .catch(err => {
-            console.log('ERROR: ' + err.message);
-            res.sendStatus(500);
-        })
+const getPastes = async (req, res) => {
+    Paste.find({})
+    .then(result => {
+        res.json(result);
     })
+    .catch(err => {
+        console.log('There was an Error: ' + err.message);
+        res.sendStatus(500);
+    });
 };
 
-module.exports = { newPaste };
+module.exports = { getPastes };
