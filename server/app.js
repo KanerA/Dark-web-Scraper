c704a92ca459bb973e55785c363ac8979f1b4e20
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-12 21:22:13 +0300

    basic GET route for server
    to check if the mongo container is connected correctly to the scraper and server

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const express = require('express');
-const checkDuplicates = require('./middlewares/checkDuplicates');
-const { newPaste } = require('./utils');
 const app = express();
+const Paste = require('./models/Paste');
 
 app.use(express.json());
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+    Paste.find({})
+    .then(result => {
+        res.json(result);
+    })
+    .catch(err => {
+        console.log(err.message)
+        res.sendStatus(500)
+    })
+});
 
 module.exports = app;
