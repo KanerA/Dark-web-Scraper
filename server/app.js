cd2eb08e96ee416f6ea12a565feece1528705d64
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-12 19:06:36 +0300

    remove the post route from app.js
    because scraper separated from server, this route is no longer needed.

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const checkDuplicates = require('./middlewares/checkDuplicates');
 const { newPaste } = require('./utils');
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {});
-
-app.post('/paste', newPaste);
 
 module.exports = app;
