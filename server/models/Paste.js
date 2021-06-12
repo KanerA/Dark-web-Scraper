7e147d5b207770358c701dc4fa5656f6359ce3a6
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-12 19:05:28 +0300

    Update Paste model in server

--- server/models/Paste.js
+++ server/models/Paste.js
@@ -4,7 +4,7 @@ const pasteSchema = new mongoose.Schema({
     author: {type: String, required: true},
     title: {type: String, required: true},
     content: [String],
-    date: {type: Date, default: new Date}
+    date: {type: Date, default: new Date, index: true}
 });
 
 module.exports = mongoose.model('Paste', pasteSchema);
