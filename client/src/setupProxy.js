b23e4372af9dbf083d03a7495b2dc35e86eee1d2
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-13 09:11:50 +0300

    Update setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,8 @@ module.exports = function(app) {
   app.use(
     '/paste',
     createProxyMiddleware({
-      target: 'http://node-scraper-server:8080',
+      target: 'http://node-scraper-server:8080', // uncomment for docker
+      // target: 'http://localhost:8081',
       changeOrigin: true,
     })
   );
