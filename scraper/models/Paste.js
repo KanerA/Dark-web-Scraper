a15decca5ed0232658c3602516e5e2582691c6ae
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-13 15:19:11 +0300

    change Paste model
    for the ability to hide pastes onClick

--- scraper/models/Paste.js
+++ scraper/models/Paste.js
@@ -4,7 +4,8 @@ const pasteSchema = new mongoose.Schema({
     author: {type: String, required: true},
     title: {type: String, required: true},
     content: [String],
-    date: {type: Date, default: new Date, index: true}
+    date: {type: Date, default: new Date, index: true},
+    hidden: {type: Boolean, default: false},
 });
 
 module.exports = mongoose.model('Paste', pasteSchema);
