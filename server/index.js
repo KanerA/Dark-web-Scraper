6b284856121704922340aef5ca59ccf0c334fd92
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-13 09:11:40 +0300

    Update index.js - Server

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 const app = require("./app");
 const mongoose = require("mongoose");
 const env = process.env.NODE_ENV || "development";
-// const MONGO_URI = process.env.MONGO_URI;
-const MONGO_URI = 'mongodb://mongo:27017/stronghold-scraper';
-const PORT = process.env.PORT || 8080;
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 8081; 
+// const MONGO_URI = 'mongodb://mongo:27017/stronghold-scraper'; //for docker use
+// const PORT = process.env.PORT || 8080;  // for docker use
 
 mongoose
   .connect(MONGO_URI, {
