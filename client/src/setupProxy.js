6381e4446eade5063d5993cc0a1fd4a75be25857
Assaf Kaner <kaner.assaf@gmail.com>
2021-09-20 17:34:27 +0300

    add proxy for Flask server

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,8 +4,9 @@ module.exports = function(app) {
   app.use(
     '/paste',
     createProxyMiddleware({
-      target: 'http://node-scraper-server:8080', // uncomment for docker
-      // target: 'http://localhost:8081',
+      // target: 'http://node-scraper-server:8080', // uncomment for docker
+      target: "http://localhost:8081", // to run locally
+      // target: "http://localhost:5000", // to run with python flask server
       changeOrigin: true,
     })
   );
